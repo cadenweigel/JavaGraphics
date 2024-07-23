a9a1eb2e787d3316cb920b498f3f959bcf45ac7f
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2024-07-22 19:19:49 -0700

    renamed file

---
+++ life.java
@@ -0,0 +1,12 @@
+
+import java.awt.Graphics2D; 
+@SuppressWarnings("unused")
+
+public class life{
+
+    public static void main(String args[]){
+        System.out.println("Hello!");
+    }
+
+}
+
